12b0838d71108158e540c0367480bf91065ad4d8
Otavio Ragazzo <otavio@65a.com.br>
2025-02-07 14:57:56 -0300

    feat: implement gRPC Todo service with PostgreSQL backend

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,12 +1,89 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"os"
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	pb "github.com/oragazzo/todo_grpc/proto"
 )
 
-func main() {
-	fmt.Println("I'm a client!")
+const (
+	port = ":8080"
+)
 
-	os.Exit(0)
+func CreateTodo(client pb.TodoServiceClient) {
+	reqCreateTodo := &pb.CreateTodoRequest{
+		Title:     "Recording",
+		Completed: false,
+	}
+
+	resCreateTodo, err := client.CreateTodo(context.Background(), reqCreateTodo)
+	if err != nil {
+		log.Fatal("Error to create todo", err.Error())
+	}
+
+	fmt.Printf("Created Todo: ID=%d, Title=%s, Completed=%t\n", resCreateTodo.Id, resCreateTodo.Title, resCreateTodo.Completed)
+}
+
+func GetTodos(client pb.TodoServiceClient) {
+	reqGetTodos := &pb.GetTodosRequest{}
+
+	resGetTodos, err := client.GetTodos(context.Background(), reqGetTodos)
+	if err != nil {
+		log.Fatal("Error to get todos", err.Error())
+	}
+
+	for _, todo := range resGetTodos.TodoList {
+		fmt.Printf("Todo: ID=%d, Title=%s, Completed=%t\n", todo.Id, todo.Title, todo.Completed)
+	}
+}
+
+func UpdateTodo(client pb.TodoServiceClient) {
+	reqUpdateTodo := &pb.UpdateTodoRequest{
+		Id:        1,
+		Title:     "Studying",
+		Completed: true,
+	}
+
+	resUpdateTodo, err := client.UpdateTodo(context.Background(), reqUpdateTodo)
+	if err != nil {
+		log.Fatal("Error to update todo", err.Error())
+	}
+
+	fmt.Printf("Updated Todo: ID=%d, Title=%s, Completed=%t\n", resUpdateTodo.Id, resUpdateTodo.Title, resUpdateTodo.Completed)
+}
+
+func DeleteTodo(client pb.TodoServiceClient) {
+	reqDeleteTodo := &pb.DeleteTodoRequest{
+		Id: 2,
+	}
+
+	resDeleteTodo, err := client.DeleteTodo(context.Background(), reqDeleteTodo)
+	if err != nil {
+		log.Fatal("Error to delete todo", err.Error())
+	}
+
+	fmt.Printf("Deleted Todo: ID=%d", reqDeleteTodo.Id)
+	fmt.Println(resDeleteTodo)
+}
+
+func main() {
+	// Set up a connection to the server
+	conn, err := grpc.Dial("localhost"+port, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalln("Error in dial", err.Error())
+	}
+	defer conn.Close()
+
+	// Create a new TodoService client
+	client := pb.NewTodoServiceClient(conn)
+
+	CreateTodo(client)
+	// GetTodos(client)
+	// UpdateTodo(client)
+	// DeleteTodo(client)
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,132 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"net"
 	"os"
+
+	"google.golang.org/grpc"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+
+	"github.com/joho/godotenv"
+	pb "github.com/oragazzo/todo_grpc/proto"
 )
 
-func main() {
-	fmt.Println("I'm a server!")
+const (
+	port = ":8080"
+)
 
-	os.Exit(0)
+type Todo struct {
+	gorm.Model
+	Title     string `gorm:"not null"`
+	Completed bool   `gorm:"not null"`
+}
+
+type server struct {
+	db *gorm.DB
+	pb.UnimplementedTodoServiceServer
+}
+
+func (s *server) CreateTodo(ctx context.Context, req *pb.CreateTodoRequest) (*pb.CreateTodoResponse, error) {
+	todo := &Todo{
+		Title:     req.Title,
+		Completed: req.Completed,
+	}
+
+	s.db.Create(todo)
+
+	return &pb.CreateTodoResponse{
+		Id:        int64(todo.ID),
+		Title:     todo.Title,
+		Completed: todo.Completed,
+	}, nil
+}
+
+func (s *server) GetTodos(ctx context.Context, req *pb.GetTodosRequest) (*pb.GetTodosResponse, error) {
+	var todos []*Todo
+
+	s.db.Find(&todos)
+
+	var resTodos []*pb.GetTodosResponse_Todo
+	for _, todo := range todos {
+		resTodo := &pb.GetTodosResponse_Todo{
+			Id:        int64(todo.ID),
+			Title:     todo.Title,
+			Completed: todo.Completed,
+		}
+		resTodos = append(resTodos, resTodo)
+	}
+
+	return &pb.GetTodosResponse{
+		TodoList: resTodos,
+	}, nil
+}
+
+func (s *server) UpdateTodo(ctx context.Context, req *pb.UpdateTodoRequest) (*pb.UpdateTodoResponse, error) {
+	var todo Todo
+
+	s.db.First(&todo, req.Id)
+	if todo.ID == 0 {
+		return nil, fmt.Errorf("Todo not found")
+	}
+
+	todo.Title = req.Title
+	todo.Completed = req.Completed
+
+	s.db.Save(&todo)
+
+	return &pb.UpdateTodoResponse{
+		Id:        int64(todo.ID),
+		Title:     todo.Title,
+		Completed: todo.Completed,
+	}, nil
+}
+
+func (s *server) DeleteTodo(ctx context.Context, req *pb.DeleteTodoRequest) (*pb.DeleteTodoResponse, error) {
+	var todo Todo
+
+	s.db.First(&todo, req.Id)
+	if todo.ID == 0 {
+		return nil, fmt.Errorf("Todo not found")
+	}
+
+	s.db.Delete(&todo)
+
+	return &pb.DeleteTodoResponse{}, nil
+}
+
+func main() {
+	// Load environment variable
+	godotenv.Load()
+
+	// Connect to the PostgreSQL database
+	db, err := gorm.Open(postgres.Open(os.Getenv("DSN")), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("failed to connect to database: %v", err)
+	}
+	log.Println("Database connecting is success")
+
+	// Automigrate the Todo struct to the PostgreSQL database
+	db.AutoMigrate(&Todo{})
+
+	// Create a new server
+	grpcServer := grpc.NewServer()
+
+	// Register TodoService server
+	pb.RegisterTodoServiceServer(grpcServer, &server{db: db})
+
+	// Start listening on port
+	listen, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalln("error to listen", err.Error())
+	}
+	log.Printf("Server listening on port localhost%v", port)
+
+	// Serve gRPC requests
+	if err := grpcServer.Serve(listen); err != nil {
+		log.Fatalln("error when serve grpc", err.Error())
+	}
 }
